2d24e7a5e7cce259359f2e10d2c31004b576cb5d
eomttt <hyunt0413@gmail.com>
2020-04-03 11:41:31 +0900

    Unuse cover

--- server.util.js
+++ server.util.js
@@ -71,37 +71,20 @@ const getBookInfo = async (fileName) => {
   }
 };
 
-const getCoverImage = (bookCover, fileName) => (
-  new Promise((resolve, reject) => {
-    if (bookCover) {
-      const imagefileNameArr = bookCover.href.split('/');
-      const imageFileName = imagefileNameArr[imagefileNameArr.length - 1];
-      fs.copyFile(`${EPUB_UNZIP_PATH}/${fileName}/${bookCover.href}`, `public/cover/${imageFileName}`, (error) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(`cover/${imageFileName}`);
-      });
-    } else {
-      resolve(DEFAULT_COVER_IMAGE);
-    }
-  })
-);
-
-const getTitle = (book) => book.creators.reduce((acc, cur, index) => `${acc}${index > 0 ? ', ' : ''}${cur.name}`, '');
+const getTitle = (creators) => creators.reduce((acc, cur, index) => `${acc}${index > 0 ? ', ' : ''}${cur.name}`, '');
 
 const getBookListItem = async (fileName) => {
   try {
     const epubFile = await getEpubFile(fileName || 'jikji');
     if (epubFile) {
-      const bookInfo = await getBookInfo(fileName);
-      const imageFileName = await getCoverImage(bookInfo.book.cover, fileName);
-      const title = getTitle(bookInfo.book);
+      const { book } = await getBookInfo(fileName);
+      const { creators, cover } = book;
+
       fs.unlinkSync(`public/${fileName}.epub`);
       return {
         fileName,
-        title,
-        coverImage: imageFileName,
+        title: getTitle(creators),
+        coverImage: cover ? `epub/${fileName}/${cover.href}` : DEFAULT_COVER_IMAGE,
       };
     }
     return null;
