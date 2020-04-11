dd040ac13b593eadc7c8d256865730fd29181ef6
eomttt <hyunt0413@gmail.com>
2020-04-11 10:27:08 +0900

    Delete unuse modules

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import fetch from 'cross-fetch';
 import { EpubBookViewer } from '../interfaces/books';
 
 export const fetchGetBook = async (fileName: string): Promise<EpubBookViewer | null> => {
   try {
-    const res = await axios.get(`/book?fileName=${fileName}`);
-    const { data, status } = res;
+    const res = await fetch(`/book?fileName=${fileName}`);
+    const { status } = res;
     if (status === 200) {
-      const { book } = data;
+      const { book } = await res.json();
       return book;
     }
   } catch (error) {
@@ -18,10 +18,13 @@ export const fetchGetBook = async (fileName: string): Promise<EpubBookViewer | n
 
 export const fetchUploadEpub = async (file: FormData): Promise<string | null> => {
   try {
-    const res = await axios.post('/upload-epub', file);
-    const { data, status } = res;
+    const res = await fetch('/upload-epub', {
+      method: 'post',
+      body: file,
+    });
+    const { status } = res;
     if (status === 200) {
-      const { fileName } = data;
+      const { fileName } = await res.json();
       return fileName;
     }
   } catch (error) {
